92a561ec89a4c54859a355d4fcb1cf54ba53ee37
Andrés López <andreslp08@hotmail.com>
2021-09-28 00:12:58 -0500

    new method to bring to front a modal

--- src/core/EpicModalManager.js
+++ src/core/EpicModalManager.js
@@ -1,7 +1,10 @@
+import { DEFAULT_Z_INDEZ } from "./Constants.js";
 import EpicModal from "./EpicModal.js";
 import EpicModalRef from "./EpicModalRef.js";
 
 let modals = [];
+
+let zIndex = DEFAULT_Z_INDEZ;
 
 const addModal = (modal) => {
     if (!modal && modal instanceof EpicModal == false ) { return };
@@ -11,6 +14,26 @@ const addModal = (modal) => {
 const removeModal = (id = "") => {
     if (!id) { return };
     modals = modals.filter(modal => modal.id != id);
+}
+
+const bringToFront =(modal)=>{
+    if (!modal && modal instanceof EpicModal == false && !modal.element) { return };
+    modals.forEach(m=>{
+        if( m.element){
+            m.element.style.zIndex = `${zIndex-1}`;
+        }
+    })
+    modal.element.style.zIndex = `${zIndex}`;
+}
+
+const setZindex =(z_index = DEFAULT_Z_INDEZ)=>{
+    zIndex = z_index;
+    modals.forEach(m=>{
+        if( m.element){
+            m.element.style.zIndex = `${zIndex}`;
+        }
+    })
+
 }
 
 const all = () => {
@@ -31,5 +54,7 @@ const all = () => {
 export {
     addModal,
     removeModal,
-    all
+    all,
+    bringToFront,
+    setZindex
 };
