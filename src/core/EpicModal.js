d0d6cbf28931841704943b1da4b13570ec8df72a
Andrés López <andreslp08@hotmail.com>
2021-09-30 23:27:48 -0500

    now EpicModal class extends from BaseEpicModal, updateConfig method was refactored as changeProperties, config renamed as DEFAULT_INITIAL_PROPERTIES and element renamed as domElementReference, finally, class was improved

--- src/core/EpicModal.js
+++ src/core/EpicModal.js
@@ -1,83 +1,54 @@
-import  * as EpicModalManager  from "./EpicModalManager.js";
-import defaultConfig from "./DefaultConfig.js";
-import { buildTemplate } from "./TemplatesManager.js";
+
+import BaseEpicModal from "./BaseEpicModal.js";
+import {buildTemplate} from "./TemplatesManager.js";
+import { DATASET_KEYS, DEFAULT_INITIAL_PROPERTIES, ERROR, HTML_DATA_KEYS, THEMES } from "./Constants.js";
 import ElementProperties from "./ElementProperties.js";
-import { DATASET_KEYS, HTML_DATA_KEYS, THEMES } from "./Constants.js";
+import { EpicModalManager } from "../index.js";
 
-export default class EpicModal {
-    static defaultIdNum = 1;
-
-    id = "";
-    config = defaultConfig;
-    element = document.createElement("div");
-
-    constructor(id = "",config = defaultConfig, parentElement = document.body) {
-        if (!id || id.trim().length <= 0) {
-            this.id = `epic-modal-${EpicModal.defaultIdNum}`;
-            EpicModal.defaultIdNum++;
-        } else {
-            this.id = id;
-        }
+export  default class EpicModal extends BaseEpicModal {
+    constructor(id = "",properties = DEFAULT_INITIAL_PROPERTIES, parentElement = document.body) {
+        super(id)
         this.parentElement = parentElement;
-        this.config = {...defaultConfig,...config};
-        this.closeButtonHandler = this.closeButtonHandler.bind(this);
+        this.properties = {...DEFAULT_INITIAL_PROPERTIES,...properties};
         this.build();
-        EpicModalManager.addModal(this)
     }
 
     build() {
         if(this.parentElement instanceof Element == false || !this.parentElement ){
-            console.error("[Epic-Modal]:'build modal' failed, parent element is not a HTMLElement or is undefined.")
-            return;
+            throw new Error(ERROR+"An error was ocurred building the epic modal, parent element is not a HTMLElement or is undefined.")
         }
-        const template =  buildTemplate(this.id, this.config);
+
+        const template =  buildTemplate(this.id, this.properties);
         this.parentElement.insertAdjacentHTML('beforeend',template )
-        if(this.element){
-            this.element.remove();
+        if(this.domElementReference){
+            this.domElementReference.remove();
         }
-        this.element = document.body.querySelector(`[${HTML_DATA_KEYS.id} = "${this.id}"]`)
-        this.updateConfig(this.config)
-        this.element.removeEventListener("click", this.closeButtonHandler, false);
-        this.element.addEventListener('click', this.closeButtonHandler);
+        this.domElementReference = document.body.querySelector(`[${HTML_DATA_KEYS.id} = "${this.id}"]`)
+        this.changeProperties(this.properties)
+        super.build();
     }
 
-    updateConfig(config = defaultConfig) {
-        const previousConfig = this.config;
-        this.config = { ...defaultConfig, ...this.config, ...config };
-        this.element.dataset[DATASET_KEYS.config] = JSON.stringify(this.config);
-        ElementProperties.setTheme(this.element, this.config.theme)
-        ElementProperties.setPosition(this.element, this.config.position)
-        ElementProperties.setBackdropClick(this.element, this.config.backdropClick);
-        if (this.config.theme != THEMES["custom"]) {
-            ElementProperties.setTitle(this.element, this.config.title);
-            ElementProperties.setContent(this.element,this.config.content);
+    changeProperties(properties = DEFAULT_INITIAL_PROPERTIES) {
+        const previousProperties = this.properties;
+        this.properties = { ...DEFAULT_INITIAL_PROPERTIES, ...this.properties, ...properties };
+        this.domElementReference.dataset[DATASET_KEYS.properties] = JSON.stringify(this.properties);
+        ElementProperties.setTheme(this.domElementReference, this.properties.theme)
+        ElementProperties.setPosition(this.domElementReference, this.properties.position)
+        ElementProperties.setBackdropClick(this.domElementReference, this.properties.backdropClick);
+        if (this.properties.theme != THEMES["custom"]) {
+            console.log("custom")
+            ElementProperties.setTitle(this.domElementReference, this.properties.title);
+            ElementProperties.setContent(this.domElementReference,this.properties.content);
         }else{
-            ElementProperties.setCustomTemplate(this.element, this.config.customTemplate);
+            
+            ElementProperties.setCustomTemplate(this.domElementReference, this.properties.customTemplate);
         }
-        if(this.config.theme != previousConfig.theme){
+        if(this.properties.theme != previousProperties.theme){
             this.refresh();
         } 
     }
 
-    refresh(){
-        this.build();
-    }
 
-    closeButtonHandler(e) {
-        if (e.target.dataset[DATASET_KEYS.closeButton] == "true" ||
-         (this.config.backdropClick == true && e.target == this.element)) {
-            this.hide()
-        }
-    }
-
-    show() {
-        ElementProperties.setOpenAnimation(this.element, this.config.openAnimation);
-        EpicModalManager.bringToFront(this);
-    }
-    hide() {
-        ElementProperties.setCloseAnimation(this.element, this.config.closeAnimation);
-    }
-    getId() {
-        return this.id;
-    }
 }
+
+
