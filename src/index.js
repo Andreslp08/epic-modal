e021c5ab2c3923cbc5ec43fcb32501ff1ecddd48
Andrés López <andreslp08@hotmail.com>
2021-09-30 23:35:39 -0500

    now exports CustomEpicModalRef

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as EpicModalManager from "./core/EpicModalManager.js"
 import EpicModal from "./core/EpicModal.js"
 import EpicModalRef from "./core/EpicModalRef.js"
+import CustomEpicModalRef from "./core/CustomEpicModalRef.js"
 
-
-export  {EpicModalManager, EpicModal, EpicModalRef}
+export  {EpicModalManager, EpicModal, EpicModalRef, CustomEpicModalRef};
