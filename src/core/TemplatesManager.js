45e6a3cc75daa9748bac7eb554cc50a4d950d5bf
Andrés López <andreslp08@hotmail.com>
2021-10-01 14:11:28 -0500

    buildTemplate improved

--- src/core/TemplatesManager.js
+++ src/core/TemplatesManager.js
@@ -3,21 +3,17 @@ import { DEFAULT_INITIAL_PROPERTIES, THEMES } from "./Constants.js";
 const buildTemplate = (id = "", properties = DEFAULT_INITIAL_PROPERTIES) => {
     if (!id && id.length <= 0) { return undefined };
     let template = undefined;
-    switch (properties.theme) {
-        case THEMES["default-light"]:
-            template = defaultTemplate(id, properties);
-            break;
-            case THEMES["default-dark"]:
-            template = defaultTemplate(id, properties);
-            break;
-            case THEMES["custom"]:
-            template = customTemplate(id, properties);
-            break;
-        default:
-            properties.theme = THEMES["default-light"];
-            template = defaultTemplate(id, properties);
-            break;
+    if (properties.theme != THEMES["custom"] && THEMES[properties.theme]) {
+        template = defaultTemplate(id, properties);
     }
+    else if (properties.theme == THEMES["custom"] && THEMES[properties.theme]) {
+        template = customTemplate(id, properties);
+    }
+    else {
+        properties.theme = THEMES["default-light"];
+        template = defaultTemplate(id, properties);
+    }
+
     return template;
 }
 
